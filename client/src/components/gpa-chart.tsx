d1d4cbf361ff94b7f9b9a127f299d70da949af5a
Liam <liam@scalzulli.com>
2026-01-12 19:27:20 -0500

    Move type out

--- client/src/components/gpa-chart.tsx
+++ client/src/components/gpa-chart.tsx
@@ -11,8 +11,20 @@ import {
 } from 'recharts';
 
 import type { Grade, TermAverage } from '../lib/term-average';
-import { gradeToGPA } from '../lib/term-average';
 import { compareTerms } from '../lib/utils';
+
+const gradeToGPA: Record<Grade, number> = {
+  A: 4.0,
+  'A-': 3.7,
+  'B+': 3.3,
+  B: 3.0,
+  'B-': 2.7,
+  'C+': 2.3,
+  C: 2.0,
+  'C-': 1.7,
+  D: 1.0,
+  F: 0,
+};
 
 type GPAChartProps = {
   averages: TermAverage[];
