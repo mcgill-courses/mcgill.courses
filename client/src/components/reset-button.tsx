865445d8f074d34ad3655e81307ec1dd2dbd9424
Jeff Zhang <zhangjeff2014@gmail.com>
2026-01-22 12:04:48 -0500

    Adjust explore filter styles (#902)

--- client/src/components/reset-button.tsx
+++ client/src/components/reset-button.tsx
@@ -15,14 +15,14 @@ export const ResetButton = ({
   return (
     <div
       className={twMerge(
-        'flex h-8 w-8 items-center justify-center rounded-full transition duration-200 hover:bg-gray-100 dark:hover:bg-neutral-700',
+        'flex h-8 w-8 cursor-pointer items-center justify-center rounded-full transition duration-200 hover:bg-gray-100 dark:hover:bg-neutral-700',
         className ?? ''
       )}
     >
       <button onClick={onClear}>
         <RefreshCw
           size={size}
-          className={'text-gray-500 dark:text-neutral-400'}
+          className={'cursor-pointer text-gray-500 dark:text-neutral-400'}
         />
       </button>
     </div>
