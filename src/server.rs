446f2b943079d54ca0c004bdcea3caf4b8e3a077
liam <liam@scalzulli.com>
2026-01-29 22:36:30 -0500

    Use assets prefix for API page custom html (#935)

--- src/server.rs
+++ src/server.rs
@@ -157,12 +157,12 @@ impl Server {
           <html lang="en">
           <head>
             <meta charset="UTF-8"/>
-            <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96"/>
-            <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
-            <link rel="shortcut icon" href="/favicon.ico"/>
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" href="/assets/favicon-96x96.png" sizes="96x96"/>
+            <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg"/>
+            <link rel="shortcut icon" href="/assets/favicon.ico"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png"/>
             <meta name="apple-mobile-web-app-title" content="mcgill.courses"/>
-            <link rel="manifest" href="/site.webmanifest"/>
+            <link rel="manifest" href="/assets/site.webmanifest"/>
             <meta name="msapplication-TileColor" content="#da532c"/>
             <meta name="theme-color" content="#ffffff"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
