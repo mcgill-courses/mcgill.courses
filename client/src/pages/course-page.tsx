dfc70eb967b961dff4b1a92fd9e6474b838198c4
Ben Khant <ak058@bucknell.edu>
2026-07-20 23:39:26 -0400

    fix: prevent search from affecting course ratings chart

--- client/src/pages/course-page.tsx
+++ client/src/pages/course-page.tsx
@@ -264,7 +264,7 @@ export const CoursePage = () => {
       </Helmet>
 
       <div className='mx-auto mt-10 max-w-6xl md:mt-0'>
-        <CourseInfo course={course} reviews={showingReviews} />
+        <CourseInfo course={course} reviews={allReviews ?? []} />
         <div className='py-2.5' />
         <div className='hidden gap-x-6 lg:grid lg:grid-cols-5'>
           <div className='col-span-3'>
